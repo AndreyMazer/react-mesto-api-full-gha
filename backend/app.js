137dc2a853d53c79f7ee3682ff349302a0ec41f3
AndreyMazer <pyshnograyandrey@yandex.ru>
2023-11-15 22:55:06 +0300

    1511

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const { errors } = require("celebrate");
 const router = require("./routes/index");
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1/mestodb" } = process.env;
+const { PORT = 3001, DB_URL = "mongodb://127.0.0.1/mestodb" } = process.env;
 
 const app = express();
 app.use(express.json());
