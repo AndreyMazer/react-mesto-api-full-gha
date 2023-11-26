5ed61fbcb51d1cf21c5b275b2b9d37ac3fdc92ec
AndreyMazer <pyshnograyandrey@yandex.ru>
2023-11-26 12:33:36 +0300

    26/11

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
-  res.status(err.statusCode).send({
+  res.status(statusCode).send({
     message: statusCode === 500 ? "На сервере произошла ошибка" : message,
   });
   return next();
