b5deb48eb9ccfd47708aa5c1384d8a501a9c6240
AndreyMazer <pyshnograyandrey@yandex.ru>
2023-11-27 20:50:36 +0300

    2711

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -100,7 +100,7 @@ const updateProfile = (req, res, next) => {
 
 const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findAndUpdate(
+  User.findByIdAndUpdate(
     req.user._id,
     { avatar },
     { new: true, runValidators: true }
