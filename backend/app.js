468b9ff4ef64e80364c3544cf6dad2d779995fe3
AndreyMazer <pyshnograyandrey@yandex.ru>
2023-11-29 18:57:58 +0300

    2911

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,17 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
-
+const winston = require("winston");
+const expressWinston = require("express-winston");
 const router = require("./routes/index");
 
 const { PORT = 3000, DB_URL = "mongodb://127.0.0.1/mestodb" } = process.env;
 
 const app = express();
+const reqLog = expressWinston.logger({
+  transports: [new winston.transports.File({ filename: "request.log" })],
+  format: winston.format.json(),
+});
 
 app.use(reqLog);
 app.use(express.json());
@@ -20,6 +25,12 @@ app.use(cookieParser());
 mongoose.connect(DB_URL);
 
 app.use(router);
+
+
+const errLog = expressWinston.errorLogger({
+  transports: [new winston.transports.File({ filename: "error.log" })],
+  format: winston.format.json(),
+});
 
 app.use(errLog);
 
@@ -36,3 +47,8 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+module.exports = {
+  reqLog,
+  errLog,
+};
